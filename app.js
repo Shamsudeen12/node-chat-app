e84c49b8fa46ed4bf52fcffc7308a48a844088f0
Shamsudeen Adedokun <adedokunshamsudeen@gmail.com>
2018-08-03 00:34:08 +0100

    Add new message and create message events

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express'),
+      socketIO = require('socket.io'),
+      http     = require('http'),
       app     = express();
 
 app.use(express.static(`${__dirname}/public`));
 const port = process.env.PORT || 1337;
-// app.get('/', (req, res) => {
+var server = http.createServer(app);
+var io     = socketIO(server);
 
-// });
+io.on('connection',(socket) => {
+  console.log('New User Connected');
 
-app.listen(port, () => console.log(`Chat app running on port ${port}`));
+  socket.emit('newMessage', {
+    from: 'mike@example.com',
+    text: 'Hey, what\'s going on',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (createMessage) => {
+    console.log(createMessage);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () => console.log(`Chat app running on port ${port}`));
