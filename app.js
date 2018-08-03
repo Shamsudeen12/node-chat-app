9da68f42767768ad26ee104e97facec7680348da
Shamsudeen Adedokun <adedokunshamsudeen@gmail.com>
2018-08-03 01:05:18 +0100

    Greet new user and alert others

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var io     = socketIO(server);
 io.on('connection',(socket) => {
   console.log('New User Connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user just joined'
+  });
+
   socket.on('createMessage', (message) => {
     console.log(message);
     io.emit('newMessage', { 
@@ -18,6 +28,11 @@ io.on('connection',(socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
