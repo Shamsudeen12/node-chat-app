4a1ae8cc38c9a9c8877250fefd1fc9207d8cf83b
Shamsudeen Adedokun <adedokunshamsudeen@gmail.com>
2018-08-03 10:58:59 +0100

    Added form user interface

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,28 @@
 var socket = io();
+
 socket.on('connect', function () {
   console.log("Connected to server");
+});
 
-  socket.on('disconnect', function () {
-    console.log("Disconnected from server");
-  });
+socket.on('disconnect', function () {
+  console.log("Disconnected from server");
+});
 
-  socket.on('newMessage', function (email) {
-    console.log('New Email', email);
+socket.on('newMessage', function (message) {
+  console.log('New Email', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
+
+$('#message-form').on("submit", function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+
   });
 });
