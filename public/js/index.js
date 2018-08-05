3672cd35bb0ee081a8c6364fccac76d824367669
Shamsudeen Adedokun <adedokunshamsudeen@gmail.com>
2018-08-05 12:51:19 +0100

    Scroll to bottom automatically

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@ var socket = io();
 var locationBtn = $('#send-location');
 var messageTextbox = $('[name=message]');
 
+function scrollToBottom () {
+  // Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop    = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log("Connected to server");
 });
@@ -19,6 +35,7 @@ socket.on('newMessage', function (message) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -30,6 +47,7 @@ socket.on('newLocationMessage', function(message) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on("submit", function (e) {
